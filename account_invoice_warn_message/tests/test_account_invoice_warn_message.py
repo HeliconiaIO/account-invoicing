ee78d8075de92d2f1c1f9c9ee1e0758d161f2a15
Bhavesh Heliconia <oca@heliconia.in>
2024-12-30 18:36:42 +0530

    [MIG] account_invoice_warn_message: Migration to 18.0

--- account_invoice_warn_message/tests/test_account_invoice_warn_message.py
+++ account_invoice_warn_message/tests/test_account_invoice_warn_message.py
@@ -89,3 +89,99 @@ class TestAccountInvoiceWarnMessage(TransactionCase):
             }
         )
         self.assertEqual(invoice.invoice_warn_msg, self.warn_msg_parent)
+
+    def test_compute_invoice_warn_msg_in_invoice(self):
+        invoice = self.env["account.move"].create(
+            {
+                "move_type": "in_invoice",
+                "partner_id": self.partner.id,
+                "invoice_line_ids": [
+                    (
+                        0,
+                        0,
+                        {
+                            "product_id": self.env.ref("product.product_product_4").id,
+                            "quantity": 1,
+                            "price_unit": 42,
+                        },
+                    ),
+                ],
+            }
+        )
+        self.assertFalse(invoice.invoice_warn_msg)
+
+    def test_compute_invoice_warn_msg_posted_state(self):
+        invoice = self.env["account.move"].create(
+            {
+                "move_type": "out_invoice",
+                "partner_id": self.partner.id,
+                "invoice_line_ids": [
+                    (
+                        0,
+                        0,
+                        {
+                            "product_id": self.env.ref("product.product_product_4").id,
+                            "quantity": 1,
+                            "price_unit": 42,
+                        },
+                    ),
+                ],
+            }
+        )
+        invoice.action_post()
+        self.assertFalse(invoice.invoice_warn_msg)
+
+    def test_compute_invoice_warn_msg_no_partner(self):
+        invoice = self.env["account.move"].create(
+            {
+                "move_type": "out_invoice",
+                "invoice_line_ids": [
+                    (
+                        0,
+                        0,
+                        {
+                            "product_id": self.env.ref("product.product_product_4").id,
+                            "quantity": 1,
+                            "price_unit": 42,
+                        },
+                    ),
+                ],
+            }
+        )
+        self.assertFalse(invoice.invoice_warn_msg)
+
+    def test_compute_invoice_warn_msg_no_warnings(self):
+        partner_no_warn = self.env["res.partner"].create(
+            {
+                "name": "Customer without warning",
+                "email": "customer@nowarn.com",
+                "invoice_warn": "no-message",
+            }
+        )
+        parent_no_warn = self.env["res.partner"].create(
+            {
+                "name": "Parent without warning",
+                "email": "parent@nowarn.com",
+                "invoice_warn": "no-message",
+            }
+        )
+        partner_no_warn.parent_id = parent_no_warn.id
+
+        invoice = self.env["account.move"].create(
+            {
+                "move_type": "out_invoice",
+                "partner_id": partner_no_warn.id,
+                "invoice_line_ids": [
+                    (
+                        0,
+                        0,
+                        {
+                            "product_id": self.env.ref("product.product_product_4").id,
+                            "quantity": 1,
+                            "price_unit": 42,
+                        },
+                    ),
+                ],
+            }
+        )
+        self.assertFalse(invoice.invoice_warn_msg)
